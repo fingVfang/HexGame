ab58cec0ba4517f45a2c486777e7e0487deadbb5
Ahmet <116463483+fingVfang@users.noreply.github.com>
2024-05-25 18:11:11 +0300

    Update HexGame.java

--- src/main/java/com/example/hexgame/HexGame.java
+++ src/main/java/com/example/hexgame/HexGame.java
@@ -11,7 +11,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
